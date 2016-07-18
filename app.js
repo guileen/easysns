1ff100f94bc5c3f047a553f4f5335a643732cd20
Gui Lin <guileen@gmail.com>
2016-07-19 01:22:24 +0800

    add simple routes

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 var http = require('http')
+var parseUrl = require('url').parse
+
+function homeController(req, res) {
+  res.end('home')
+}
+
+function userController(req, res) {
+  res.end('user')
+}
+
+function notFoundController(req, res) {
+  res.writeHead(404)
+  res.end('Not found')
+}
+
+const rules = [
+  {path: '/', controller: homeController},
+  {path: '/user', controller: userController}
+]
+
+function find(ary, match) {
+  for(var i = 0; i< ary.length; i++) {
+    if(match(ary[i])) return ary[i]
+  }
+}
 
 var server = http.createServer(function(req, res) {
-  res.end('hello world\n')
+  var urlInfo = parseUrl(req.url)
+  var rule = find(rules, function(rule) {
+    return rule.path == urlInfo.pathname 
+  })
+  var controller = rule && rule.controller || notFoundController
+  controller(req, res)
 })
 
 server.listen(3000)
