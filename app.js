fedab3255a77807b6e16fd531f914aad011d00a7
Gui Lin <guileen@gmail.com>
2016-07-21 02:05:41 +0800

    refactor controllers; support RegExp router

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
 var http = require('http')
+var controllers = require('./controllers')
 var parseUrl = require('url').parse
 
-function homeController(req, res) {
-  res.end('home')
-}
-
-function userController(req, res) {
-  res.end('user')
-}
+const rules = [
+  {path: '/', controller: controllers.home},
+  {path: '/user', controller: controllers.user},
+  {path: /^\/static(\/.*)/, controller: controllers.static},
+]
 
 function notFoundController(req, res) {
   res.writeHead(404)
   res.end('Not found')
 }
-
-const rules = [
-  {path: '/', controller: homeController},
-  {path: '/user', controller: userController}
-]
 
 function find(ary, match) {
   for(var i = 0; i< ary.length; i++) {
@@ -28,6 +22,9 @@ function find(ary, match) {
 var server = http.createServer(function(req, res) {
   var urlInfo = parseUrl(req.url)
   var rule = find(rules, function(rule) {
+    if (rule.path instanceof RegExp) {
+      return urlInfo.pathname.match(rule.path)
+    }
     return rule.path == urlInfo.pathname 
   })
   var controller = rule && rule.controller || notFoundController
---
+++ controllers/index.js
@@ -0,0 +1,4 @@
+exports.home = require('./home')
+exports.user = require('./user')
+exports.static = require('./static')
+
